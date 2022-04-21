f5bb9d10266855a6ad8d9a3299224fa6e8f05871
zhangxunhui <zhangxunhui@nudt.edu.cn>
2022-04-21 15:27:48 +0800

    update shell script

--- services/restart_services.sh
+++ services/restart_services.sh
@@ -1,21 +1,25 @@
-#! /bin/bash
+#!/bin/bash
 
-function restart_service {
-    old_pid=$(ps ax|grep $1.py|grep -v grep|awk '{print $1}')
+restart_service()
+{
+    service_name=$1
+    script_name="$service_name.py"
+    log_name="$service_name.log"
+    old_pid=$(ps ax|grep $script_name|grep -v grep|awk '{print $1}')
     echo "old_pid=${old_pid}"
     if [ -z $old_pid ];then
         echo "Process Non-existent!"
         echo "Starting Process...."
-        nohup python $1.py > $1.log 2>&1 &
+        nohup python $script_name > $log_name 2>&1 &
     else
         kill -9 ${old_pid}
         mid_pid=$(ps ax|grep $1.py|grep -v grep|awk '{print $1}')
         if [ -z ${mid_pid} ];then
             echo "Process Close Success!"
             echo "Start Restarting....."
-            nohup python $1.py > $1.log 2>&1 &
+            nohup python $script_name > $log_name 2>&1 &
         else
-            echo "Process Close Fail !"
+            echo "Process Close Fail!"
             exit 1
         fi
     fi
@@ -25,6 +29,7 @@ function restart_service {
     else
         echo "Restart Success!"
         echo "new_pid=${new_pid}"
+    fi
 }
 
-restart_service CodeStartPerception
+restart_service 'CodeStartPerception'
