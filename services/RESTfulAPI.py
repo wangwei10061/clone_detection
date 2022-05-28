753357d417aac8c4f9719ab970109532a0f1435c
zhangxunhui <zhangxunhui@nudt.edu.cn>
2022-05-28 09:36:45 +0800

    添加了flask restful API的框架

---
+++ services/RESTfulAPI.py
@@ -0,0 +1,16 @@
+# aim: this is used to define the restful api for clients
+# date: 2022-05-28
+# author: zhangxunhui
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
