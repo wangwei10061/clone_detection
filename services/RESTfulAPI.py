f81741f7c2ffb159865c4e4cd120e5fd04ec4162
zhangxunhui <zhangxunhui@nudt.edu.cn>
2022-05-30 15:50:01 +0800

    修改了RESTful API

--- services/RESTfulAPI.py
+++ services/RESTfulAPI.py
@@ -3,8 +3,9 @@
 # author: zhangxunhui
 
 import os
-from difflib import SequenceMatcher
 
+from ChangedFuncExtractor import ChangedFuncExtractor
+from dulwich.diff_tree import tree_changes
 from dulwich.repo import Repo
 from flask import Flask, request
 from MySQLUtils import MySQLUtils
@@ -12,103 +13,144 @@ from utils import read_config
 
 app = Flask(__name__)
 
-config_path = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "config.yml"
-)
-config = read_config(config_path)
 
-mysql_utils = MySQLUtils(
-    host=config["mysql"]["host"],
-    port=config["mysql"]["port"],
-    username=config["mysql"]["username"],
-    password=config["mysql"]["password"],
-    database=config["mysql"]["database"],
-    autocommit=False,
-    dictcursor=True,
-)
+class HandlePR(object):
+    def __init__(
+        self,
+        base_repo_id: int,
+        head_repo_id: int,
+        base_commit_sha: str,
+        head_commit_sha: str,
+    ):
 
+        self.base_repo_id = base_repo_id
+        self.head_repo_id = head_repo_id
+        self.base_commit_sha = base_commit_sha
+        self.head_commit_sha = head_commit_sha
 
-def extract_diff(old_content: list, new_content: list):
-    changed_new_lines = []  # record all the changed lines for the new file
-    for tag, _, _, j1, j2 in SequenceMatcher(
-        None, old_content, new_content
-    ).get_opcodes():
-        if tag == "equal":
-            pass
-        elif tag == "insert" or tag == "replace":
-            changed_new_lines.extend([i for i in range(j1 + 1, j2 + 1)])
-        elif tag == "delete":
-            pass
+        self.config_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "config.yml"
+        )
+        self.config = read_config(self.config_path)
+
+        self.mysql_utils = MySQLUtils(
+            host=self.config["mysql"]["host"],
+            port=self.config["mysql"]["port"],
+            username=self.config["mysql"]["username"],
+            password=self.config["mysql"]["password"],
+            database=self.config["mysql"]["database"],
+            autocommit=False,
+            dictcursor=True,
+        )
+
+        """Query mysql for owner_name and name of repository."""
+        base_repo_info = self.mysql_utils.get_repo_info(
+            repo_id=self.base_repo_id
+        )
+        base_repo_ownername = base_repo_info["owner_name"]
+        base_repo_reponame = base_repo_info["name"]
+        self.base_repo_path = os.path.join(
+            self.config["gitea"]["repositories_path"],
+            base_repo_ownername,
+            base_repo_reponame + ".git",
+        )
+        self.base_repo = Repo(self.base_repo_path)
+
+        head_repo_info = self.mysql_utils.get_repo_info(
+            repo_id=self.head_repo_id
+        )
+        head_repo_ownername = head_repo_info["owner_name"]
+        head_repo_reponame = head_repo_info["name"]
+        self.head_repo_path = os.path.join(
+            self.config["gitea"]["repositories_path"],
+            head_repo_ownername,
+            head_repo_reponame + ".git",
+        )
+        self.head_repo = Repo(self.head_repo_path)
+
+    def extract_changed_funcs(self):
+
+        """Compare the diff between head_commit_sha and base_commit_sha.
+        There are two cases:
+        1. the head branch is not generated from base branch - base_commit_sha is not in the head repo, because using --allow-unrelated-histories
+        2. the head branch is generated from base branch - base_commit_sha is in the head repo
+        """
+
+        if self.base_commit_sha.encode() not in self.head_repo:
+            # this is the first case
+            """The files in head repo are all newly added."""
+            t_changes = tree_changes(
+                self.head_repo.object_store,
+                tree1_id=None,
+                tree2_id=self.head_repo.object_store[
+                    self.head_commit_sha.encode()
+                ].tree,
+            )
         else:
-            raise Exception("type error")
-    return changed_new_lines
+            # this is the second case
+            t_changes = tree_changes(
+                self.head_repo.object_store,
+                tree1_id=self.head_repo.object_store[
+                    self.base_commit_sha.encode()
+                ].tree,
+                tree2_id=self.head_repo.object_store[
+                    self.head_commit_sha.encode()
+                ].tree,
+            )
 
+        changed_funcs = []
+        for t_change in t_changes:
+            changed_funcs.extend(
+                ChangedFuncExtractor(t_change, self.config).parse()
+            )
+        return changed_funcs
 
-def extract_changed_funcs(
-    repo: Repo, base_commit_sha: str, new_commit_shas: list
-):
-    all_parents = []  # store all the parent commit shas
-    for new_commit_sha in new_commit_shas:
-        commit = repo.object_store[new_commit_sha.encode()]
-        parents = commit.parents
-        parents = [parent.decode() for parent in parents]
-        all_parents.extend(parents)
-    # find the new_commits that are not regarded as parent
-    lastest_new_commits = [
-        new_commit_sha
-        for new_commit_sha in new_commit_shas
-        if new_commit_sha not in all_parents
-    ]
-
-    # compare the diff between lastest_new_commits and base_commit_sha
-    # for
-    # changed_new_lines = self.extract_diff(
-    #     old_content=self.repo.object_store[
-    #         tree_change.old.sha
-    #     ].data.split(b"\n"),
-    #     new_content=self.repo.object_store[
-    #         tree_change.new.sha
-    #     ].data.split(b"\n"),
-    # )
-    print(lastest_new_commits)
+    def parse(self):
+        """Find changed functions."""
+        changed_funcs = self.extract_changed_funcs()
+        """Do the clone detection."""
+        for changed_func in changed_funcs:
+            print("pause")
 
 
 @app.route("/clone_detection", methods=["POST"])
-def clone_detection():
+def clone_detection_api():
     data = request.get_json()
     if (
-        "repo_id" not in data
-        or "new_commit_shas" not in data
+        "base_repo_id" not in data
+        or "head_repo_id" not in data
+        or "head_commit_sha" not in data
         or "base_commit_sha" not in data
     ):
         return "RESTful request error: repo_id or new_commit_shas or base_commit_sha parameter not found!"
     else:
-        repo_id = data["repo_id"]
-        if type(repo_id) != int:
-            return "RESTful request error: repo_id should be an integer!"
+        base_repo_id = data["base_repo_id"]
+        if type(base_repo_id) != int:
+            return "RESTful request error: base_repo_id should be an integer!"
 
-        new_commit_shas = data["new_commit_shas"]
-        if type(new_commit_shas) != list:
-            return "RESTful request error: new commit shas should be a list!"
+        head_repo_id = data["head_repo_id"]
+        if type(head_repo_id) != int:
+            return "RESTful request error: head_repo_id should be an integer!"
+
+        head_commit_sha = data["head_commit_sha"]
+        if type(head_commit_sha) != str:
+            return "RESTful request error: new commit shas should be a string!"
 
         base_commit_sha = data["base_commit_sha"]
         if type(base_commit_sha) != str:
             return "RESTful request error: old commit sha should be a string!"
 
-        """Query mysql for owner_name and name of repository."""
-        repo_info = mysql_utils.get_repo_info(repo_id=repo_id)
-        ownername = repo_info["owner_name"]
-        reponame = repo_info["name"]
-        repo_path = os.path.join(
-            config["gitea"]["repositories_path"], ownername, reponame + ".git"
-        )
-        repo = Repo(repo_path)
-        print(str(repo))
+        HandlePR(
+            base_repo_id=base_repo_id,
+            head_repo_id=head_repo_id,
+            base_commit_sha=base_commit_sha,
+            head_commit_sha=head_commit_sha,
+        ).parse()
 
         """Find changed files and related changed methods."""
 
         """Extract the n-grams."""
-        return new_commit_shas
+        return head_commit_sha
 
 
 if __name__ == "__main__":
