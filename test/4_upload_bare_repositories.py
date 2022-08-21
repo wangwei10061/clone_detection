25c6a4e6c381942cf8acbb8236ff71cc0665e3d9
zhangxunhui <zhangxunhui@nudt.edu.cn>
2022-08-21 18:53:39 +0800

    update .gitignore

--- test/4_upload_bare_repositories.py
+++
@@ -1,9 +0,0 @@
-# aim: upload the bare repositories for simulation
-# author: zhangxunhui
-# date: 2022-07-15
-
-from utils import upload_projects
-
-upload_projects()
-
-print("finish uploading projects!")
--- test/download_projects.py
+++
@@ -1,7 +0,0 @@
-# aim: download github java projects to test the server performance
-# author: zhangxunhui
-# date: 2022-06-21
-
-
-if __name__ == "__main__":
-    print("finish")
